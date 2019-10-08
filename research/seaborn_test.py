ce9d84862a18e3a6f74bb6e3a7491a8af8f04925
Mark Raymond Jr <markrjr@live.com>
2019-10-08 15:58:51 -0500

    Basic collection of accelerometer data in different scenarios.

--- research/seaborn_test.py
+++ research/seaborn_test.py
@@ -2,11 +2,14 @@ import pandas as pd
 import seaborn as sns
 from matplotlib import pyplot as plt
 
-sensor_data = pd.read_csv('Accel_Readings.csv')
+sensor_data = pd.read_csv('Accel_Cheek_PSU_Walk.csv')
 print(sensor_data.head(5))
 
 sensor_data['TIMESTAMP'] = pd.to_datetime(sensor_data['TIMESTAMP'])
 print(sensor_data.info())
 
-sns.lineplot(x=sensor_data['TIMESTAMP'], y=sensor_data["Y"]).set_title("Sensor Readings")
+sns.lineplot(x=sensor_data['TIMESTAMP'], y=sensor_data["Y"], label="Y Accel").set_title("Accel Readings")
+sns.lineplot(x=sensor_data['TIMESTAMP'], y=sensor_data["X"], label="X Accel")
+sns.lineplot(x=sensor_data['TIMESTAMP'], y=sensor_data["Z"], label="Z Accel")
+
 plt.show()
