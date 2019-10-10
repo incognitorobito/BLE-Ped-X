7f7104e5f1e2904d6add8f6ed55eb01e159f50eb
Mark Raymond Jr <markrjr@live.com>
2019-10-09 20:29:04 -0500

    Manually register converters.

--- research/seaborn_test.py
+++ research/seaborn_test.py
@@ -1,12 +1,15 @@
 import pandas as pd
+import numpy as np
 import seaborn as sns
 from matplotlib import pyplot as plt
+from pandas.plotting import register_matplotlib_converters
 
-sensor_data = pd.read_csv('Accel_Pocket_Curb_Stomp.csv')
+register_matplotlib_converters()
+
+sensor_data = pd.read_csv('Accel_PSU_Short_Pauses.csv')
 print(sensor_data.head(5))
 
 sensor_data['TIMESTAMP'] = pd.to_datetime(sensor_data['TIMESTAMP'])
-print(sensor_data.info())
 
 sns.lineplot(x=sensor_data['TIMESTAMP'], y=sensor_data["Y"], label="Y Accel").set_title("Accel Readings")
 sns.lineplot(x=sensor_data['TIMESTAMP'], y=sensor_data["X"], label="X Accel")
